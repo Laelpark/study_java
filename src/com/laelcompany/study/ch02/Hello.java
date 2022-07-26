0815374047cc6bd44ecf7da4643649ee3e54df10
Laelpark <gottkfkd@naver.com>
2022-07-26 16:15:03 +0900

    2022-07-26

---
+++ src/com/laelcompany/study/ch02/Hello.java
@@ -0,0 +1,38 @@
+package com.laelcompany.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+//		데이터타입 변수명(마음대로 짓기) = (초기)값;  -> 꼭 외우기
+//		데이터타입 변수명
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+		int width = 100;
+
+		String id = "gottkfkd";
+		int password = 100;
+		String name = "박라엘";
+		String gender = "여자";
+		String email = "gottkfkd@naver.com";
+		String phonenumber = "010-3977-7694";	
+		String place = "장소";
+		String price = "12,000원";
+		
+	
+		System.out.println("id 변수에 들어가 있는 값은: " + id);
+		System.out.println("password 변수에 들어가 있는 값은: " + password);
+		System.out.println("gender 변수에 들어가 있는 값은: " + gender);
+		System.out.println("email 변수에 들어가 있는 값은: " + email);
+		System.out.println("phonnumber 변수에 들어가 있는 값은: " + phonenumber);
+		System.out.println("place 변수에 들어가 있는 값은: " + place);
+		System.out.println("price 변수에 들어가 있는 값은: " + price);
+		System.out.println("name 변수에 들어가 있는 값은: " + name);
+						// 데이터 값 안에 한번 넣어 보고 싶은 값은 ""치고 안에 넣기 
+						// 넣은 값 확인하는 법 ctrl+f11
+		
+	}
+}
