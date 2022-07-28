b1562987d2c4801ab920433691a6c73aaf630e4f
Laelpark <gottkfkd@naver.com>
2022-07-28 17:25:26 +0900

    2022-07-28

---
+++ src/com/laelcompany/study/ch02/CharExample.java
@@ -0,0 +1,24 @@
+package com.laelcompany.study.ch02;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		
+//		char 타입을 사용시에는 작은 따음표 사용
+//		자바는 모든 문자를 유니코드로 처리함. 국제 표준 규약
+		
+		char c1 = 'A';          // 문자를 저장
+		char c2 = 65;           // 십진수로 저장
+		char c3 = '\u0041';     // 16진수로 저장
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		
+//		char c4 = "";
+//		char c5 = "";
+//		char c6 = "";
+		
+		
+	}
+}
