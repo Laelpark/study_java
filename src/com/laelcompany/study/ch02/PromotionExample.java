20b2b98883b6c2e50f10798b69182eb77565c32d
Laelpark <gottkfkd@naver.com>
2022-08-01 17:28:19 +0900

    2022-08-01

--- src/com/laelcompany/study/ch02/PromotionExample.java
+++ src/com/laelcompany/study/ch02/PromotionExample.java
@@ -4,6 +4,7 @@ public class PromotionExample {
 
 	public static void main(String[] args) {
 	
+//		자동 타입 변환 (promotion)
 //		작은 형이 큰형으로 변환은 문제가 발생하지 않는다.
 //		큰 형이 작은 형으로 변하면 문제가 생기거나 변환이 이루어져도 데이터 손실이 발생한다.
 //		byte < short < int < long < float < double
@@ -32,6 +33,24 @@ public class PromotionExample {
 		
 		double doubleValue = intValue3;
 		System.out.println("doubleValue:" + doubleValue + " 입니다.");
+		
+//      int에 short 대입 후 출력
+		short shortValue = 10;
+		int intValue1 = shortValue;
+		System.out.println("inttValue: " + intValue1);
+		
+//      long에 int 대입 후 출력	
+		long longValue = intValue1;
+		System.out.println("longValue: " + longValue);
+		
+//      float에 long 대입 후 출력	
+		float floatValue = longValue;
+		System.out.println("floatValue: " + floatValue);
+		
+//      double에 float 대입 후 출력	
+		double doubleValue1 = floatValue;
+		System.out.println("doubleValue1: " + doubleValue1);
+		
 		
 	}
 }
