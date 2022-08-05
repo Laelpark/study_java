92d678ca417f840dd1d6f49c72f5974dfe589079
Laelpark <gottkfkd@naver.com>
2022-08-05 17:31:12 +0900

    2022-08-05

---
+++ src/com/laelcompany/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,38 @@
+package com.laelcompany.study.ch03;
+
+public class AssignmentOperatorExample {
+	
+	public static void main(String[] args) {
+		
+//		+=
+		
+		int result = 0;
+		int result2 = 0;
+		int result3 = 0;
+		
+		
+		result += 10;
+//		result = result + 10;
+		
+		System.out.println("result: " + result);
+		
+//		-=
+
+		result2 -= 10;
+//		result2 = result2 - 10;
+		
+		System.out.println("result: " + result2);	
+		
+//		*=
+		
+
+		result3 *= 10;
+//		result2 = result3 * 10;
+		
+		System.out.println("result: " + result3);
+		
+		
+	}
+	
+}
+
