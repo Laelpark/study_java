2cf4ccda4f28f005dc8940f4c7291c3beaedd879
Laelpark <gottkfkd@naver.com>
2022-08-10 17:21:51 +0900

    2022-08-10

--- src/com/laelcompany/study/ch04/RandomSum.java
+++ src/com/laelcompany/study/ch04/RandomSum.java
@@ -1,5 +1,7 @@
 package com.laelcompany.study.ch04;
 
+import java.util.Random;
+
 public class RandomSum {
 	
 	public static void main(String[] args) {
@@ -52,29 +54,29 @@ public class RandomSum {
 		
 //		여기서 부터 정민님 소스
 		
-//		Random rand = new Random();
-//		
-//		int num1 = rand.nextInt(100);
-//		int num2 = rand.nextInt(100);
-//		
-//		int sum = 0;
-//		
-//		if(num1 > num2) {
-//			for(int i = num2 ; i <= num1; i++) {
-//				sum += i;
-//				System.out.print(i + " + ");
-//				if(i == num1) System.out.print(i + " ");
-//			}
-//			System.out.print(" = " + sum);
-//		} else {
-//			for(int i = num1 ; i <= num2; i++) {
-//				sum += i;
-//				System.out.print(i + " + ");
-//				if(i == num2) System.out.print(i + " ");
-//			}
-//			System.out.print(" = " + sum);
-////			System.out.println(num1 + " ~ " + num2 + " 사이의 합은 " + sum);
-//		}
+		Random rand = new Random();
+		
+		int num1 = rand.nextInt(100);
+		int num2 = rand.nextInt(100);
+		
+		int sum = 0;
+		
+		if(num1 > num2) {
+			for(int i = num2 ; i <= num1; i++) {
+				sum += i;
+				System.out.print(i + " + ");
+				if(i == num1) System.out.print(i + " ");
+			}
+			System.out.print(" = " + sum);
+		} else {
+			for(int i = num1 ; i <= num2; i++) {
+				sum += i;
+				System.out.print(i + " + ");
+				if(i == num2) System.out.print(i + " ");
+			}
+			System.out.print(" = " + sum);
+			System.out.println(num1 + " ~ " + num2 + " 사이의 합은 " + sum);
+		}
 		
 			
 	}
