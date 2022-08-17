5d2ca6b44499f4ab08370b9b52dce4e49b3984a6
Laelpark <gottkfkd@naver.com>
2022-08-17 14:32:10 +0900

    2022-08-17

--- src/com/laelcompany/study/ch03/StringConcatExample.java
+++ src/com/laelcompany/study/ch03/StringConcatExample.java
@@ -7,7 +7,7 @@ public class StringConcatExample {
 //		우리가 사용하는 jdk 버전은 1.11이다. 
 		
 		String name = "jdk";
-		String num1 = 1.11;
+		String num1 = "1.11";
 		
 		System.out.println("우리가 사용하는" + name + "버전은" + num1 + "이다.");
 		
